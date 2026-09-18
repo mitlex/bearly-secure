124f54ab767ac739f39ccab8b8c0bc6d24494e04
mitlex <94235400+mitlex@users.noreply.github.com>
2026-09-18 14:39:16 +0100

    feat: normalize email addresses to trim outer whitespace and lowercase all chars

--- internal/accounts/store.go
+++ internal/accounts/store.go
@@ -10,6 +10,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"strings"
 	"time"
 
 	"github.com/bootdotdev/learn-web-security/internal/database/dbgen"
@@ -20,7 +21,9 @@ const defaultSessionTTL = 30 * 24 * time.Hour
 var ErrEmailExists = errors.New("an account already exists for that email")
 
 func NormalizeEmail(email string) string {
-	return email
+	trimmedEmail := strings.TrimSpace(email)
+	lowerCaseAndTrimmedEmail := strings.ToLower(trimmedEmail)
+	return lowerCaseAndTrimmedEmail
 }
 
 type User struct {
